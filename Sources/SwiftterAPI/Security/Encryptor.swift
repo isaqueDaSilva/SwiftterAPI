2c74d87bd8c8cccd7096313b9064507803345048
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-05-22 20:56:37 -0300

    Creating a Encryptor type This type contains one method that is responsible to encrypt a field.

--- Sources/SwiftterAPI/Security/Encryptor.swift
+++ Sources/SwiftterAPI/Security/Encryptor.swift
@@ -5,4 +5,26 @@
 //  Created by Isaque da Silva on 5/22/25.
 //
 
-import Foundation
+import Vapor
+
+enum Encryptor {
+    /// Encrypts an field using a shared key.
+    /// - Parameters:
+    ///   - field: The field data representation that will be encrypted.
+    ///   - privateKey: Some private key used to calculate a shared key.
+    ///   - publicKey: Some public key used to calculate a shared key.
+    /// - Returns: Returns a Data representation of the encrypted field.
+    static func encrypts(
+        _ field: Data,
+        with privateKey: PrivateKey,
+        and publicKey: PublicKey
+    ) async throws -> Data {
+        let encryptedField = try await CryptographyHandler.encryptField(
+            field,
+            with: privateKey,
+            and: publicKey
+        )
+        
+        return encryptedField
+    }
+}
