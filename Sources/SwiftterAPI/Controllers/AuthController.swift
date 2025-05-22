961aba1c4115811dbeb48a81cc75c1589eac64b8
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-05-22 03:24:44 -0300

    Updating AuthController In this update I finish the signup method by adding a calculation process to create both access tokens and refresh token.

--- Sources/SwiftterAPI/Controllers/AuthController.swift
+++ Sources/SwiftterAPI/Controllers/AuthController.swift
@@ -5,6 +5,7 @@
 //  Created by Isaque da Silva on 5/21/25.
 //
 
+import JWT
 import Vapor
 
 struct AuthController: RouteCollection {
@@ -21,16 +22,20 @@ extension AuthController {
     @Sendable
     private func signUp(with request: Request) async throws -> SignUpResponse {
         let createUserDTO = try request.content.decode(CreateUser.self)
-        let newUser = try await UserServices.create(with: createUserDTO, request: request)
+        let newUser = try await UserService.create(with: createUserDTO, request: request)
         let userSlug = try await SlugGenerator.generate(for: newUser.name, with: request.db)
-        let newProfile = try await UserProfileServices.create(
+        let newProfile = try await UserProfileService.create(
             with: userSlug,
             userID: newUser.requireID(),
             at: request.db
         )
         
-        // TODO: - Create the JWT -
+        let accessPayload = try Payload(with: newUser.requireID(), userSlug: userSlug, audienceType: .fullAccess)
+        let accessToken = try await request.jwt.sign(accessPayload, kid: JWTSecretIdentifier.accessTokenSecreyKey.key)
         
-        return try .init(accessToken: "", refreshToken: "", userProfile: newProfile.toDTO())
+        let refreshPayload = try Payload(with: newUser.requireID(), userSlug: userSlug, audienceType: .refresh)
+        let refreshToken = try await request.jwt.sign(accessPayload, kid: JWTSecretIdentifier.refreshTokenSecreyKey.key)
+        
+        return try .init(accessToken: accessToken, refreshToken: refreshToken, userProfile: newProfile.toDTO())
     }
 }
