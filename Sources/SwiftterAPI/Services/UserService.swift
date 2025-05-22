a6e69b95ff09cf27c301b055994be5c3994b970e
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-05-22 20:11:08 -0300

    Small changes at UserService type.

--- Sources/SwiftterAPI/Services/UserService.swift
+++ Sources/SwiftterAPI/Services/UserService.swift
@@ -11,12 +11,12 @@ enum UserService {
     /// Creates a new user at the database.
     /// - Parameter request: The main request object that is responsible to perform the operation.
     /// - Returns: Returns an instance of the generated user.
-    static func create(with createUserDTO: CreateUser, request: Request) async throws -> User {
-        try CreateUser.validate(content: request)
+    static func create(with createUserDTO: CreateUserRequest, request: Request) async throws -> User {
+        try CreateUserRequest.validate(content: request)
         
         let password = try await Decryptor.decryptField(
             createUserDTO.password,
-            with: createUserDTO.publicKey
+            with: createUserDTO.publicKeyForPassword
         )
         
         guard PasswordChecker.check(password) else {
