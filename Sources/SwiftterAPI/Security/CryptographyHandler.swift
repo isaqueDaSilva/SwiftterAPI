b6a27215049e049737a1e8e7ee8eb3b2dc9c147c
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-05-22 20:55:48 -0300

    CryptographyHandler update In this update the name, before called SecureCommunicationHandler, and now CryptographyHandler. Also, I add a encrypt method, that helps to encrypt a field using a shared key.

--- Sources/SwiftterAPI/Security/CryptographyHandler.swift
+++ Sources/SwiftterAPI/Security/CryptographyHandler.swift
@@ -1,5 +1,5 @@
 //
-//  SecureCommunicationHandler.swift
+//  CryptographyHandler.swift
 //  SwiftterAPI
 //
 //  Created by Isaque da Silva on 5/21/25.
@@ -12,32 +12,26 @@ typealias PrivateKey = P384.KeyAgreement.PrivateKey
 typealias PublicKey = P384.KeyAgreement.PublicKey
 
 /// A handler object that is responsable to execute functions releted of key exchange and decrytion.
-enum SecureCommunicationHandler {
+enum CryptographyHandler {
     
     /// Generates a pair of server's public and private key.
     /// - Returns: Returns a server public key
     ///
     /// When the server generate a pair of key, the private key is stored locally on a server's cache,
     /// and the public key is sent to user, to perform a encryption process.
-    static func generatesPublicKey() async throws -> ECPublicKey {
+    static func generatesPublicKey() async throws -> ECKeyPair {
         let privateKey = PrivateKey()
         let label = UUID()
         await SecureKeysCache.shared.add(privateKey, for: label)
         let publicKey = privateKey.publicKey.rawRepresentation
-        return .init(id: label, publicKey: publicKey)
+        return .init(privateKeyID: label, publicKey: publicKey)
     }
     
     /// Performs a calculation to generate a shared key, between serve's private key and client's public key, to decrypt a given encrypted field.
     /// - Parameter clientKey: The given client's public key.
     /// - Returns: Returns a shared key value to decrypt the field.
-    static func generateSharedKey(for clientKey: ECPublicKey) async throws -> SymmetricKey {
-        guard let serverPrivateKey = await SecureKeysCache.shared[clientKey.id] else {
-            throw Abort(.internalServerError, reason: "No Server's private key available.")
-        }
-        
-        let clientPublicKey = try PublicKey(rawRepresentation: clientKey.publicKey)
-        
-        let sharedSecret = try serverPrivateKey.sharedSecretFromKeyAgreement(with: clientPublicKey)
+    static func generateSharedKey(with publicKey: PublicKey, and privateKey: PrivateKey) async throws -> SymmetricKey {
+        let sharedSecret = try privateKey.sharedSecretFromKeyAgreement(with: publicKey)
         
         let symmetricKey = sharedSecret.x963DerivedSymmetricKey(
             using: SHA512.self,
@@ -45,9 +39,25 @@ enum SecureCommunicationHandler {
             outputByteCount: 32
         )
         
-        await SecureKeysCache.shared.remove(for: clientKey.id)
-        
         return symmetricKey
+    }
+    
+    /// Encrypts a field using a share key process.
+    /// - Parameters:
+    ///   - field: The field that will be encrypted.
+    ///   - privateKey: A private key that will be used to calculate a shared key.
+    ///   - publicKey: A public key that will be used to calculate a shared key.
+    /// - Returns: Returns a Data representation of the encrypted field.
+    static func encryptField(_ field: Data, with privateKey: PrivateKey, and publicKey: PublicKey) async throws -> Data {
+        let sharedKey = try await Self.generateSharedKey(with: publicKey, and: privateKey)
+        
+        let sealedBox = try AES.GCM.seal(field, using: sharedKey, nonce: .init())
+
+        guard let encryptedToken = sealedBox.combined else {
+            throw Abort(.internalServerError, reason: "Error to generate refresh token.")
+        }
+        
+        return encryptedToken
     }
     
     /// Decrypt a given encrypted field.
