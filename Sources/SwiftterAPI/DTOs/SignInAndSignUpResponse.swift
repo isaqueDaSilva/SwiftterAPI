230abcb46b664a9071a0e8ea111ec3d925873c79
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-05-24 17:39:05 -0300

    SignInAndSignUpResponse update In this update, I add a static method that gets some useful informations to build the response for sign in and sign up process.

--- Sources/SwiftterAPI/DTOs/SignInAndSignUpResponse.swift
+++ Sources/SwiftterAPI/DTOs/SignInAndSignUpResponse.swift
@@ -16,4 +16,34 @@ struct SignInAndSignUpResponse: Content {
     let refreshToken: Data
     let serverPublicKey: Data
     let userProfile: Profile
+    
+    private init(accessToken: String, refreshToken: Data, serverPublicKey: Data, userProfile: Profile) {
+        self.accessToken = accessToken
+        self.refreshToken = refreshToken
+        self.serverPublicKey = serverPublicKey
+        self.userProfile = userProfile
+    }
+}
+
+extension SignInAndSignUpResponse {
+    static func build(
+        with userID: UUID,
+        userProfile: UserProfile,
+        clientPublicKey: Data,
+        and request: Request
+    ) async throws -> SignInAndSignUpResponse {
+        let (accessToken, refreshToken, publicKey) = try await JWTService.createPairOfJWT(
+            userID: userID,
+            userSlug: userProfile.requireID(),
+            clientPublicKeyData: clientPublicKey,
+            request: request
+        )
+        
+        return try .init(
+            accessToken: accessToken,
+            refreshToken: refreshToken,
+            serverPublicKey: publicKey,
+            userProfile: userProfile.toDTO()
+        )
+    }
 }
