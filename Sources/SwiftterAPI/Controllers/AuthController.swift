a3e2df23ccaa357e4a3b54d3bb99b6ccd284b115
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-05-27 22:32:00 -0300

    AuthController update Adding one final method, the sign out method, where here, it's capable to revoke the access and the tokens of the user.

--- Sources/SwiftterAPI/Controllers/AuthController.swift
+++ Sources/SwiftterAPI/Controllers/AuthController.swift
@@ -13,10 +13,12 @@ struct AuthController: RouteCollection, ProtectedRouteProtocol {
         let authRoute = routes.grouped("auth")
         let userProtectedRoute = userProtectedRoute(with: authRoute)
         let refreshTokenBasedRoute = authRoute.grouped(VerifyAndDisableTokensMiddleware())
+        let tokenProtectedRoute = self.tokenProtectedRoute(with: authRoute)
         
         authRoute.post("signup") { try await self.signUp(with: $0) }
         userProtectedRoute.post("signin") { try await self.signIn(with: $0) }
         refreshTokenBasedRoute.put("refresh-token") { try await self.refreshToken(with: $0) }
+        tokenProtectedRoute.delete("signout") { _ in "Hello" }
     }
 }
 
@@ -51,18 +53,23 @@ extension AuthController {
         
         guard let userProfile = user.profile else { throw Abort(.unauthorized) }
         
-        return try await .build(
+        let response = try await SignInResponse.build(
             with: user.requireID(),
             userProfile: userProfile,
             clientPublicKey: clientPublicKey,
             and: request
         )
+        
+        try await user.updateLoggedStatus(with: true, on: request.db)
+        
+        return response
     }
     
     @Sendable
     private func refreshToken(with request: Request) async throws -> TokenPair {
         let refreshTokenPayload = try request.auth.require(Payload.self)
-        guard let clientPublicKeyData = try await request.cache.get(KeyCollection.storageKey, as: Data.self) else {
+        
+        guard let clientPublicKeyData = try await request.cache.get(FieldsForTokenRefresh.storageKey, as: Data.self) else {
             throw Abort(.internalServerError)
         }
         
@@ -76,5 +83,57 @@ extension AuthController {
             clientPublicKeyData: clientPublicKeyData,
             request: request
         )
+    }
+    
+    @Sendable
+    private func signOut(with request: Request) async throws -> HTTPStatus {
+        let accessTokenPayload = try request.auth.require(Payload.self)
+        
+        let accessToken: String = if let token = request.headers.bearerAuthorization?.token {
+            token
+        } else {
+            request.logger.error("No token is available at the bearer authorization header.")
+            throw Abort(.unauthorized)
+        }
+        
+        let refreshToken = if let token = request.headers.first(name: "X-Refresh-Token") {
+            token
+        } else {
+            request.logger.error("No token is available at the custom `X-Refresh-Token` header.")
+            throw Abort(.unauthorized)
+        }
+        
+        let refreshTokenPayload: Payload = if let payload = try? JWTService.getPayload(on: refreshToken.toData()) {
+            payload
+        } else {
+            try await JWTService.disableTokens(
+                accessTokenID: accessTokenPayload.jwtID.value,
+                refreshTokenID: JWTService.makeUnknownID(),
+                accessTokenValue: accessToken,
+                refreshTokenValue: refreshToken,
+                on: request.db
+            )
+            
+            throw Abort(.unauthorized)
+        }
+        
+        try await JWTService.disableTokens(
+            accessTokenID: accessTokenPayload.jwtID.value,
+            refreshTokenID: refreshTokenPayload.jwtID.value,
+            accessTokenValue: accessToken,
+            refreshTokenValue: refreshToken,
+            on: request.db
+        )
+        
+        guard let userID = UUID(accessTokenPayload.subject.value),
+              let user = try await User.find(userID, on: request.db)
+        else {
+            request.logger.error("The user cannot be founded.")
+            throw Abort(.unauthorized)
+        }
+        
+        try await user.updateLoggedStatus(with: false, on: request.db)
+        
+        return .ok
     }
 }
