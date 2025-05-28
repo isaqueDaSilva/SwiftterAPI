09fd3d790b1e038da0c20726b5bf301297999a47
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-05-27 22:32:41 -0300

    VerifyAndDisableTokensMiddleware update Adding a way to revoke the provided token when for some reason some validations fail.

--- Sources/SwiftterAPI/Middlewares/VerifyAndDisableTokensMiddleware.swift
+++ Sources/SwiftterAPI/Middlewares/VerifyAndDisableTokensMiddleware.swift
@@ -12,100 +12,75 @@ import Vapor
 struct VerifyAndDisableTokensMiddleware: AsyncMiddleware {
     func respond(to request: Request, chainingTo next: any AsyncResponder) async throws -> Response {
         let fields = try request.content.decode(FieldsForTokenRefresh.self)
-        let sharedKey = try await SharedKeyMaker.makeSharedKey(with: fields.keyPair.keyPairForDecryption)
-        let decryptedRefreshToken = try CryptographyHandler.decryptField(
-            encryptedField: fields.refreshToken.toData(),
-            key: sharedKey
+        
+        let (accessPayload, refreshPayload) = try await self.getPayloads(
+            for: fields.accessToken,
+            and: fields.refreshToken,
+            in: request
         )
         
-        guard let decryptedRefreshToken else {
-            let tokenID = try self.getPayload(on: fields.accessToken.toData()).jwtID.value
-            try await self.disableToken(with: tokenID, rawValue: fields.accessToken, at: request.db)
+        do {
+            try await JWTService.verifyClaimsAtPairOf(
+                accessTokenPayload: accessPayload,
+                refreshTokenPayload: refreshPayload,
+                at: request.db
+            )
+        } catch {
+            try await JWTService.disableTokens(
+                accessTokenID: accessPayload.jwtID.value,
+                refreshTokenID: refreshPayload.jwtID.value,
+                accessTokenValue: fields.accessToken,
+                refreshTokenValue: fields.refreshToken,
+                on: request.db
+            )
             
             throw Abort(.unauthorized)
         }
         
-        let (accessPayload, refreshPayload) = try await self.getPayloads(
-            for: fields.accessToken,
-            and: decryptedRefreshToken,
-            in: request.jwt
-        )
-        
-        try await JWTService.verifyClaimsAtPairOf(
-            accessTokenPayload: accessPayload,
-            refreshTokenPayload: refreshPayload,
-            at: request.db
-        )
-        
-        try await self.disableTokens(
+        try await JWTService.disableTokens(
             accessTokenID: accessPayload.jwtID.value,
             refreshTokenID: refreshPayload.jwtID.value,
             accessTokenValue: fields.accessToken,
-            refreshTokenValue: decryptedRefreshToken,
+            refreshTokenValue: fields.refreshToken,
             on: request.db
         )
         
         request.auth.login(refreshPayload)
-        try await request.cache.set(KeyCollection.storageKey, to: fields.keyPair.publicKeyForEncryption, expiresIn: .minutes(2))
+        
+        try await request.cache.set(
+            FieldsForTokenRefresh.storageKey,
+            to: fields.publicKeyForEncryption,
+            expiresIn: .minutes(2)
+        )
         
         return try await next.respond(to: request)
-    }
-    
-    private func getPayload(on token: Data) throws -> Payload {
-        let (_, payload, _) = try DefaultJWTParser().parse(token, as: Payload.self)
-        
-        return payload
     }
     
     private func getPayloads(
         for accessToken: String,
         and refreshToken: String,
-        in jwt: Request.JWT
+        in request: Request
     ) async throws -> (accessTokenPayload: Payload, refreshTokenPayload: Payload) {
-        async let accessTokenPayload = try self.getPayload(on: accessToken.toData())
-        async let refreshTokenPayload = try jwt.verify(refreshToken, as: Payload.self)
+        async let accessTokenPayload = try JWTService.getPayload(on: accessToken.toData())
+        async let refreshTokenPayload = try request.jwt.verify(refreshToken, as: Payload.self)
         
-        let payloads = try await [accessTokenPayload, refreshTokenPayload]
-        
-        return (accessTokenPayload: payloads[0], refreshTokenPayload: payloads[1])
-    }
-    
-    private func disableToken(with id: String, rawValue: String, at database: any Database) async throws {
-        try await JWTService.disableToken(
-            with: id,
-            tokenValue: rawValue,
-            on: database
-        )
-    }
-    
-    private func disableTokens(
-        accessTokenID: String,
-        refreshTokenID: String,
-        accessTokenValue: String,
-        refreshTokenValue: String,
-        on database: any Database
-    ) async throws {
-        try await withThrowingTaskGroup { group in
-            _ = group.addTaskUnlessCancelled {
-                try await self.disableToken(
-                    with: accessTokenID,
-                    rawValue: accessTokenValue,
-                    at: database
-                )
-            }
+        do {
+            let payloads = try await [accessTokenPayload, refreshTokenPayload]
             
-            _ = group.addTaskUnlessCancelled {
-                try await self.disableToken(
-                    with: refreshTokenID,
-                    rawValue: refreshTokenValue,
-                    at: database
-                )
-            }
+            return (accessTokenPayload: payloads[0], refreshTokenPayload: payloads[1])
+        } catch {
+            let unknownAccessTokenID = JWTService.makeUnknownID()
+            let unknownRefreshTokenID = JWTService.makeUnknownID()
             
-            guard try await group.next() != nil else {
-                group.cancelAll()
-                return
-            }
+            try await JWTService.disableTokens(
+                accessTokenID: unknownAccessTokenID,
+                refreshTokenID: unknownRefreshTokenID,
+                accessTokenValue: accessToken,
+                refreshTokenValue: refreshToken,
+                on: request.db
+            )
+            
+            throw Abort(.unauthorized)
         }
     }
 }
