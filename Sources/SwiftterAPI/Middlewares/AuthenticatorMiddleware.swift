894c11719ee3d75defd13a91fb96ff5d7bf2c923
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-05-29 16:32:31 -0300

    AuthenticatorMiddleware update Adding a checker to checks if the the user is logged or not. If he's logged, an unauthorized error will be thrown.

--- Sources/SwiftterAPI/Middlewares/AuthenticatorMiddleware.swift
+++ Sources/SwiftterAPI/Middlewares/AuthenticatorMiddleware.swift
@@ -21,6 +21,10 @@ struct AuthenticatorMiddleware: AsyncBasicAuthenticator {
         
         let user = try await UserService.getUser(byEmail: basic.username, at: request.db)
         
+        guard !user.isLogged else {
+            throw Abort(.unauthorized)
+        }
+        
         guard try Bcrypt.verify(decryptedPassword, created: user.passwordHash), !user.isLogged else {
             throw Abort(.unauthorized)
         }
