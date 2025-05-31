9b5bd474d72a59a7f5748c5713e45e6bb2d85abd
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-05-30 22:57:51 -0300

    HTTPHeaders+Extension created Creating a new property inside of HTTPHeaders type from NIOHTTP1, to get automatically a refresh token from a `X-Refresh-Token` custom header.

--- Sources/SwiftterAPI/Extensions/HTTPHeaders+Extension.swift
+++ Sources/SwiftterAPI/Extensions/HTTPHeaders+Extension.swift
@@ -5,4 +5,10 @@
 //  Created by Isaque da Silva on 5/30/25.
 //
 
-import Foundation
+import NIOHTTP1
+
+extension HTTPHeaders {
+    var refreshToken: String? {
+        self.first(name: "X-Refresh-Token")
+    }
+}
