588da636830b53559e1317c162c8f71ca934be47
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-06-01 04:44:50 -0300

    Creating the UpdateType

--- Sources/SwiftterAPI/Utilities/UpdateType.swift
+++ Sources/SwiftterAPI/Utilities/UpdateType.swift
@@ -1,3 +1,11 @@
+//
+//  UpdateType.swift
+//  SwiftterAPI
+//
+//  Created by Isaque da Silva on 6/1/25.
+//
+
+/// An utility that helps to indicates what type of updation will be occur.
 enum UpdateType {
-    case increment, decrement, name(String)
+    case increment, decrement
 }
