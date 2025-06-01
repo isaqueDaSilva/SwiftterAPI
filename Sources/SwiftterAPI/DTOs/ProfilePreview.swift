746833612594323f3919dec60035d7f7e7d68e98
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-06-01 04:37:28 -0300

    Creating the ProfilePreview This type is responsible to be a short representation of a profile.

--- Sources/SwiftterAPI/DTOs/ProfilePreview.swift
+++ Sources/SwiftterAPI/DTOs/ProfilePreview.swift
@@ -5,4 +5,23 @@
 //  Created by Isaque da Silva on 6/1/25.
 //
 
-import Foundation
+import Fluent
+import Vapor
+
+struct ProfilePreview: Content {
+    let slug: String
+    let bio: String?
+    let profilePictureName: String
+}
+
+extension Array where Element == UserProfile {
+    func toPreview() throws -> [ProfilePreview] {
+        try self.map {
+            try .init(
+                slug: $0.requireID(),
+                bio: $0.bio,
+                profilePictureName: $0.profilePictureName
+            )
+        }
+    }
+}
