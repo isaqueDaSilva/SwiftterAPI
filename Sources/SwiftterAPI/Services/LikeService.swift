b1eedc0631c178dba89d71b52f20c3b9f626391a
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-06-07 03:29:53 -0300

    Creating the LikeService

--- Sources/SwiftterAPI/Services/LikeService.swift
+++ Sources/SwiftterAPI/Services/LikeService.swift
@@ -5,4 +5,19 @@
 //  Created by Isaque da Silva on 6/6/25.
 //
 
-import Foundation
+import Fluent
+import Vapor
+
+enum LikeService {
+    static func findLike(with swifeetID: String, profileSlug: String, at database: any Database) async throws -> Like? {
+        try await Like
+            .query(on: database)
+            .with(\.$swifeet)
+            .group(.and) { group in
+                group
+                    .filter(\.$id, .equal, swifeetID)
+                    .filter(\.$profile.$id, .equal, profileSlug)
+            }
+            .first()
+    }
+}
--- Sources/SwiftterAPI/Services/PictureService.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  SwiftterAPI
-//
-//  Created by Isaque da Silva on 6/6/25.
-//
-
-import Foundation
