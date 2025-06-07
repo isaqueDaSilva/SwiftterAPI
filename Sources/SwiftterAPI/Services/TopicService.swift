65cb3328601e50f06e1e41bb920aeccc1aa04aaf
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-06-07 03:27:26 -0300

    Creating TopicService A service that is responsible to add or update a topic in database!

--- Sources/SwiftterAPI/Services/TopicService.swift
+++ Sources/SwiftterAPI/Services/TopicService.swift
@@ -5,4 +5,29 @@
 //  Created by Isaque da Silva on 6/6/25.
 //
 
-import Foundation
+import Fluent
+import Vapor
+
+enum TopicService {
+    static func updateTopics(from body: String, at database: any Database) async throws {
+        let topics = TopicIdentifier.identify(at: body)
+        
+        guard !topics.isEmpty else { return }
+        
+        for topic in topics {
+            try await Self.updatePopularity(for: topic, at: database)
+        }
+    }
+    
+    static func updatePopularity(for topic: String, at database: any Database) async throws {
+        if let topic = try await Topic
+            .query(on: database)
+            .filter(\.$topic, .equal, topic)
+            .first()
+        {
+            try await topic.updateCounter(on: database)
+        } else {
+            try await Topic(topicName: topic).create(on: database)
+        }
+    }
+}
