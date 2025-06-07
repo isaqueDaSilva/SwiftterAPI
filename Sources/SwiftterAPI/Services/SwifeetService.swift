b1b6b8f5624cf3ea7433b35b4282e6667adf31fb
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-06-07 03:29:27 -0300

    Creating SwifeetService This service is responsible to reunes some important methods that is responsible to perform some important operation like create a new swifeet on database.

--- Sources/SwiftterAPI/Services/SwifeetService.swift
+++ Sources/SwiftterAPI/Services/SwifeetService.swift
@@ -5,4 +5,81 @@
 //  Created by Isaque da Silva on 6/6/25.
 //
 
-import Foundation
+import Fluent
+import Vapor
+
+enum SwifeetService {
+    static func create(
+        with dto: CreateSwifeet,
+        imageName: String?,
+        and userSlug: String,
+        at database: any Database
+    ) async throws -> Swifeet {
+        let newSwifeet: Swifeet = try await database.transaction { database in
+            let newSwifeet = Swifeet(
+                body: dto.body,
+                imageName: imageName,
+                answerOf: dto.answerOf,
+                profileSlug: userSlug
+            )
+            
+            try await newSwifeet.create(on: database)
+            
+            try await withThrowingTaskGroup { group in
+                _ = group.addTaskUnlessCancelled {
+                    let userProfile = try await UserProfileService.getProfile(by: userSlug, on: database)
+                    try await userProfile.updateSwifeetCount(with: .increment, at: database)
+                }
+                
+                _ = group.addTaskUnlessCancelled {
+                    try await Self.updateAnswersCountForOriginalSwifteet(
+                        with: newSwifeet.requireID(),
+                        at: database
+                    )
+                }
+                
+                if let body = dto.body {
+                    _ = group.addTaskUnlessCancelled {
+                        try await TopicService.updateTopics(from: body, at: database)
+                    }
+                }
+                
+                guard try await group.next() != nil else {
+                    group.cancelAll()
+                    return
+                }
+            }
+            
+            return newSwifeet
+        }
+        
+        return newSwifeet
+    }
+    
+    static func updateAnswersCountForOriginalSwifteet(
+        with swifeetID: String,
+        type: UpdateType = .increment,
+        at database: any Database
+    ) async throws {
+        let originalSwifeet = try await Swifeet.find(swifeetID, on: database)
+        
+        guard let originalSwifeet else { throw Abort(.notFound, reason: "Swifeet not available to perform action") }
+        
+        try await originalSwifeet.updateAnswersCount(with: .increment, at: database)
+    }
+    
+    static func addImageAtFileSystem(with userSlug: String, imageData: Data) async throws -> String {
+        let imageName = userSlug + "-" + Date().ISO8601Format() + "-" + "\(Int.random(in: .min ... .max))" + ".jpg"
+        let fullPath = try EnvironmentValues.swifeetPicturePath(with: imageName)
+        try await FileSystemHandler.write(.init(data: imageData), at: fullPath)
+        
+        return imageName
+    }
+    
+    static func removeImagesFromFileSystem(with imageName: String) async throws {
+        let fullPath = try EnvironmentValues.swifeetPicturePath(with: imageName)
+        guard try await FileSystemHandler.delete(at: fullPath) else {
+            throw Abort(.internalServerError)
+        }
+    }
+}
