194c3847415f145330cb4071be136115ca87f185
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-06-08 04:50:38 -0300

    SecureKeysCache update Adding a custom checker, to check if the oldest private key, stored at the cache, is expired or not. Also creating a queue that uses an ordered dictionary to stores the id of the private keys, and the expiration date as the key to check after if it was expired or not

---
+++ Sources/SwiftterAPI/Models/SwifeetTopic.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  SwiftterAPI
+//
+//  Created by Isaque da Silva on 6/7/25.
+//
+
+import Foundation
--- Sources/SwiftterAPI/Security/SecureKeysCache.swift
+++ Sources/SwiftterAPI/Security/SecureKeysCache.swift
@@ -5,6 +5,7 @@
 //  Created by Isaque da Silva on 5/21/25.
 //
 
+import Collections
 import Crypto
 import Vapor
 
@@ -13,31 +14,69 @@ final actor SecureKeysCache {
     /// A shared instance to access this cache.
     static let shared = SecureKeysCache()
     
-    private var privateKeys: [UUID: PrivateKey] = [:]
+    private var deleterTask: Task<Void, Never>? = nil
     
+    private var privateKeys: [UUID: PrivateKey] = [:]
+    private var deleterQueue: OrderedDictionary<Date, UUID> = [:]
     
     /// Adds a new key at the storage.
     /// - Parameters:
     ///   - privateKey: The private key representation that will be store.
     ///   - id: An identifier for the key.
     func add(_ privateKey: PrivateKey, for id: UUID) {
+        let twoMinutesInterval: TimeInterval = 60 * 2
+        let twoMinutesFromNow = Date().addingTimeInterval(twoMinutesInterval)
         self.privateKeys[id] = privateKey
+        self.deleterQueue[twoMinutesFromNow] = id
+        
+        if self.deleterTask == nil {
+            self.deleterTask?.cancel()
+            self.deleterTask = Task.detached(priority: .background) { await self.check() }
+        }
     }
-    
     
     /// Removes a key from the storage.
     /// - Parameter id: The id of the key that will be removed.
     func remove(for id: UUID) { privateKeys[id] = nil }
     
+    /// Schedule a periodic checker to check if the header key is expired or not, when the cache is not empty.
+    private func check() async {
+        while !self.deleterQueue.isEmpty && !self.privateKeys.isEmpty {
+            let (date, id) = (self.deleterQueue.keys.first, self.deleterQueue.values.first)
+            
+            if let date, let id, date <= Date() {
+                self.remove(for: id)
+                self.deleterQueue.removeFirst()
+            }
+            
+            try? await Task.sleep(for: .seconds(120))
+        }
+        
+        self.deleterTask?.cancel()
+        self.deleterTask = nil
+    }
+    
     subscript(_ id: UUID) -> PrivateKey? {
-        let key = privateKeys[id]
+        let key = self.privateKeys[id]
         
         if key != nil {
             self.remove(for: id)
+        }
+        
+        if privateKeys.isEmpty && deleterQueue.isEmpty {
+            self.deleterTask?.cancel()
+            self.deleterTask = nil
         }
         
         return key
     }
     
     private init() { }
+    
+    deinit {
+        self.deleterTask?.cancel()
+        self.deleterTask = nil
+        self.privateKeys.removeAll()
+        self.deleterQueue.removeAll()
+    }
 }
