0ce2986bd2f1e94d5eda010969de7e2a447bb3d3
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-06-09 03:00:47 -0300

    SlugGenerator update Updating the generate method, by now checking the cache instead the actual database, causing a performance improvement as well, when a slug is in generation.

--- Sources/SwiftterAPI/Utilities/SlugGenerator.swift
+++ Sources/SwiftterAPI/Utilities/SlugGenerator.swift
@@ -6,47 +6,29 @@
 //
 
 import Vapor
-import Fluent
 
 enum SlugGenerator {
     /// Generates a slug for an user profile, based on your name.
     /// - Parameters:
     ///   - userName: The name of the user that is creating a profile.
-    ///   - database: The database representation that will performed some checks to see if the slug already exists.
     /// - Returns: Returns a slug for the profile of the User.
     ///
     /// >Important: The slug flows the following structure: `slug-based-representation-of-the-name-of-user-222333(<-random integer)`.
-    static func generate(for userName: String, with database: any Database) async throws -> String {
+    static func generate(for userName: String) async throws -> String {
         let slug = userName.convertedToSlug()
         
         guard let slug else {
             throw Abort(.notAcceptable, reason: "Seams that the given name empty.")
         }
         
-        var userSlug = slug
+        var profileSlug = slug
         
-        while try await checkIfTheUserSlugExist(userSlug, on: database) {
+        while await SlugCache.shared.has(profileSlug) {
             let slugSufix = Int.random(in: .min ... .max)
-            let newUserSlug = userSlug + "\(slugSufix)"
-            userSlug = newUserSlug
+            let newProfileSlug = profileSlug + "\(slugSufix)"
+            profileSlug = newProfileSlug
         }
         
-        return userSlug
-    }
-    
-    /// Checks if the slug already exists.
-    /// - Parameters:
-    ///   - slug: The generated slug.
-    ///   - database: The database representation that will performed some checks to see if the slug already exists.
-    /// - Returns: A boolean value that indicates if the slug exists or not.
-    private static func checkIfTheUserSlugExist(_ slug: String, on database: any Database) async throws -> Bool {
-        guard try await UserProfile.query(on: database)
-            .filter(\.$id, .equal, slug)
-            .count() == 0
-        else {
-            return true
-        }
-        
-        return false
+        return profileSlug
     }
 }
