816b3ba797efb3a7663f7a15c05a5f25110dfca4
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-06-09 03:01:57 -0300

    UserProfileService update Updating the slug parameter name.

--- Sources/SwiftterAPI/Services/UserProfileService.swift
+++ Sources/SwiftterAPI/Services/UserProfileService.swift
@@ -11,12 +11,12 @@ import Vapor
 enum UserProfileService {
     /// Creates a new profile for an user.
     /// - Parameters:
-    ///   - userSlug: A slug value for add as identifier at the profile of the user.
+    ///   - slug: A slug value for add as identifier at the profile of the user.
     ///   - userID: The id of the user to create a relationship between the profile and user.
     ///   - database: The database client representation to mediates the communication between the API and the Database system.
     /// - Returns: Returns an instance of the created user profile.
-    static func create(with userSlug: String, userID: String, at database: any Database) async throws -> UserProfile {
-        let newProfile = UserProfile(id: userSlug, userID: userID)
+    static func create(with slug: String, userID: String, at database: any Database) async throws -> UserProfile {
+        let newProfile = UserProfile(id: slug, userID: userID)
         try await newProfile.create(on: database)
         
         return newProfile
