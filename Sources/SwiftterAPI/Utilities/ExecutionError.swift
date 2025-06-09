d1b427972af339927acaab905b37d5b3440fb478
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-06-09 04:03:11 -0300

    Creating ExecutionError A type that reunes all error types in one place.

--- Sources/SwiftterAPI/Utilities/ExecutionError.swift
+++ Sources/SwiftterAPI/Utilities/ExecutionError.swift
@@ -5,4 +5,26 @@
 //  Created by Isaque da Silva on 6/9/25.
 //
 
-import Foundation
+import Vapor
+
+enum ExecutionError: Error {
+    case notAcceptable
+    case notFound
+    case unauthorized
+    case internalServerError
+    
+    func error(with description: String? = nil) -> Abort {
+        switch self {
+        case .notAcceptable:
+            let description = description != nil ? description : "Not acceptable request."
+            return Abort(.notAcceptable, reason: description)
+        case .notFound:
+            let description = description != nil ? description : "Item not found."
+            return Abort(.notFound, reason: description)
+        case .unauthorized:
+            return Abort(.unauthorized, reason: description)
+        case .internalServerError:
+            return Abort(.internalServerError, reason: description)
+        }
+    }
+}
